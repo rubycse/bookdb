7b6e8e4d2af86157f43849ab1495d11e96d994b4
Lutfun Nahar <lutfun@therapservices.net>
2016-10-25 16:45:27 +0600

    Changed MySQL DB to Postgresql

--- src/main/bookdb.sql
+++ src/main/bookdb.sql
@@ -1,11 +1,13 @@
 CREATE TABLE book
 (
-  id BIGINT NOT NULL AUTO_INCREMENT,
+  id SERIAL NOT NULL,
   title VARCHAR(255) NOT NULL,
   author VARCHAR(255) NOT NULL,
   description VARCHAR(1000),
   file_name VARCHAR(255),
-  file MEDIUMBLOB,
-  file_size BIGINT,
+  file OID,
+  file_size Integer,
   PRIMARY KEY (id)
 );
+
+CREATE SEQUENCE hibernate_sequence;
